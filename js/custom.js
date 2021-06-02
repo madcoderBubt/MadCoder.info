96ed3cf935f0f2ccb6699b0010a69ca694c6ada7
Sudhananda Biswas <sudhananda@yahoo.com>
2021-06-03 00:36:34 +0600

    timeline & skills update

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,28 @@
-$(function() {
-    $(".pie").each(function() {
-        var percent = $(this).data("percent"),
-            $left = $(this).find(".left span"),
-            $right = $(this).find(".right span"),
-            deg;
-        
-        if(percent<=50) {
-            // Hide left
-            $left.hide();
+setTimeout(() => {
+    $(function() {
+        $(".pie").each(function() {
+            var percent = $(this).data("percent"),
+                $left = $(this).find(".left span"),
+                $right = $(this).find(".right span"),
+                deg;
             
-            // Adjust right
-            deg = 180 - (percent/100*360)
-            $right.css({
-                "-webkit-transform": "rotateZ(-"+deg+"deg)"
-            });
-        } else {
-            // Adjust left
-            deg = 180 - ((percent-50)/100*360)
-            $left.css({
-                "-webkit-transform": "rotateZ(-"+deg+"deg)"
-            });
-        }
+            if(percent<=50) {
+                // Hide left
+                $left.hide();
+                
+                // Adjust right
+                deg = 180 - (percent/100*360)
+                $right.css({
+                    "-webkit-transform": "rotateZ(-"+deg+"deg)"
+                });
+            } else {
+                // Adjust left
+                deg = 180 - ((percent-50)/100*360)
+                $left.css({
+                    "-webkit-transform": "rotateZ(-"+deg+"deg)"
+                });
+            }
+        });
     });
-});
+}, 500);
+
