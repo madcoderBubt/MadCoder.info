3fb7b8b5670468b264d85475ac0b98a9343ed0ec
Sudhananda Biswas <sudhananda@yahoo.com>
2021-06-12 18:16:28 +0600

    Resume & little changes

---
+++ js/myApp.js
@@ -0,0 +1,82 @@
+//Angular app init
+var app = angular.module('myApp', [])
+.config(['$compileProvider', function ($compileProvider) {
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|skype|javascript):/);
+}]);
+
+app.controller('myAboutCtrl', function($scope,$http) {
+    $scope.info = "";
+    $scope.objectives = "";
+    var item = null;
+
+    var url = "https://madcoderbubt.github.io/MadCoderPersonal/Data/about.json";
+    $http.get(url)
+    .then(function(response){
+        $scope.info = response.data.data.info;
+        $scope.objectives = response.data.data.objectives;
+        //console.log(response.data.data);
+    });
+    //console.log($scope.info);
+});
+app.controller('mySkillsCtrl', function($scope,$http) {
+    $scope.info = "";
+    $scope.objectives = "";
+    var item = null;
+
+    var url = "https://madcoderbubt.github.io/MadCoderPersonal/Data/skills.json";
+    $http.get(url)
+    .then(function(response){
+        $scope.highlights = response.data.skillHighlight;
+        $scope.coreSkills = response.data.coreSkills;
+        $scope.addSkills = response.data.additionalSkills;
+        //console.log(response.data.data);
+    });
+    //console.log($scope.info);
+});
+app.controller('myTimelineCtrl', function($scope,$http) {
+    $scope.info = "";
+    $scope.objectives = "";
+    var item = null;
+
+    var url = "https://madcoderbubt.github.io/MadCoderPersonal/Data/timeline.json";
+    $http.get(url)
+    .then(function(response){
+        $scope.infos = response.data.data;
+        //console.log(response.data.data);
+    });
+    //console.log($scope.info);
+});
+app.controller('ctrlProtfolio', function($scope,$http) {
+    $scope.info = "";
+    $scope.objectives = "";
+    var item = null;
+
+    var url = "https://madcoderbubt.github.io/MadCoderPersonal/Data/portfolio.json";
+    $http.get(url)
+    .then(function(response){
+        $scope.lists = response.data.data;
+        //$scope.objectives = response.data.data.objectives;
+        console.log(response.data.data);
+    });
+    //console.log($scope.info);
+});
+
+//side about me details
+app.controller('ctrlMe', function($scope,$http) {
+    $scope.info = "";
+    $scope.objectives = "";
+    var item = null;
+
+    var url = "https://madcoderbubt.github.io/MadCoderPersonal/Data/me.json";
+    $http.get(url)
+    .then(function(response){
+        $scope.info = response.data.data.info;
+        $scope.socialLinks = response.data.data.socialLinks;
+        //console.log(response.data.data);
+    });
+    //console.log($scope.info);
+});
+//manu list ctrl
+app.controller('myMenuList',function($scope,$http){
+    
+});
